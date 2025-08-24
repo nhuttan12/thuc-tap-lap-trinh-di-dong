176f307444b981087b4748601e0434ae19ba0aba
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-08-25 00:25:08 +0700

    initial mobile project, front end is kotlin

---
+++ ECommerce/app/src/main/java/com/example/e_commerce/Activity/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.e_commerce.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.e_commerce.R
+import com.example.e_commerce.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this@SplashActivity, DashboardActivity::class.java))
+        }
+    }
+}
