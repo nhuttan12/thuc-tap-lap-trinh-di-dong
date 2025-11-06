93981df2e325a0fd41c92a953497858c2a273873
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-11-06 19:30:42 +0700

    create project admin web

--- ECommerce/app/src/main/java/com/example/e_commerce/Model/ProductModel.kt
+++ ECommerce/app/src/main/java/com/example/e_commerce/Model/ProductModel.kt
@@ -1,7 +1,9 @@
 package com.example.e_commerce.Model
 
+import com.squareup.moshi.JsonClass
 import java.io.Serializable
 
+@JsonClass(generateAdapter = true)
 data class ItemModel(
     var title: String = "",
     var description: String = "",
@@ -12,4 +14,4 @@ data class ItemModel(
     var oldPrice: Double = 0.0,
     var rating: Double = 0.0,
     var numberInCart: Int = 1
-): Serializable
+) : Serializable
