06e2be001bdc0c603b46dc46d8e872313c18db74
nhuttan12 <21130530@st.hcmuaf.edu.vn>
2025-12-24 13:55:37 +0700

    modify error response interface

--- back-end/src/common/filter/error-response.interface.ts
+++ back-end/src/common/filter/error-response.interface.ts
@@ -1,3 +1,10 @@
+/**
+ * @description ErrorResponse interface
+ * @author Nhut Tan
+ * @since 2025-09-08
+ * @version 1.0.0
+ */
+
 export interface ErrorResponse {
 	statusCode: number;
 	message: string | string[];
